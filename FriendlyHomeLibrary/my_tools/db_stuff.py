ed9854b5b21a8da442238a84a42731a26a774d18
Catherine Morton <camorton2@gmail.com>
2018-05-06 13:16:01 -0400

    testing

--- FriendlyHomeLibrary/my_tools/db_stuff.py
+++ FriendlyHomeLibrary/my_tools/db_stuff.py
@@ -8,13 +8,13 @@ from FHLBuilder import models
 from FHLBuilder import choices
 
 def move_songs_to_drive(to_drive):
-    print(u'Start')
+    print(u'Start work')
     count = 0
     for current in models.Song.objects.all():
         if current.collection.drive is not to_drive:
             count = count+1
             current.collection.drive = to_drive
             current.collection.save()
-    print(u'Done')
+    print(u'Done, number of collections modified')
     print(count)
     
